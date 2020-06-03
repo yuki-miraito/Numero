0d0a5e2eff27452f151e89d282454b70352a404b
yukimasa ikeda <yyykms123@gmail.com>
2020-06-04 03:44:38 +0900

    clean: convert

--- Numero/Converter.swift
+++ Numero/Converter.swift
@@ -30,23 +30,22 @@ class Converter {
     func convert(_ number: Int) -> String {
         var result = ""
         var localNumber = number
-        while localNumber >= 10 {
-            result += "X"
-            localNumber = localNumber - 10
+        
+        let numberSymbols: [(number: Int, symbol: String)] = [
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+        
+        for item in numberSymbols {
+            while localNumber >= item.number {
+                result += item.symbol
+                localNumber = localNumber - item.number
+            }
         }
-        if localNumber >= 9 {
-            result += "IX"
-            localNumber = localNumber - 9
-        }
-        if localNumber >= 5 {
-            result += "V"
-            localNumber = localNumber - 5
-        }
-        if localNumber >= 4 {
-            result += "IV"
-            localNumber = localNumber - 4
-        }
-        result += String(repeating: "I", count: localNumber)
+        
         return result
     }
 }
