83185f3c09e9195b95ade1b8b6e3da557a1a0235
yukimasa ikeda <yyykms123@gmail.com>
2020-06-04 02:51:24 +0900

    add: ConverterTests

---
+++ NumeroTests/ConverterTests.swift
@@ -0,0 +1,26 @@
+//
+//  ConverterTests.swift
+//  NumeroTests
+//
+//  Created by yukimasa ikeda on 2020/06/04.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+    
+    let converter = Converter()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+
+}
